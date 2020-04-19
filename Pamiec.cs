775e4b22abbae9f8f4c985b95d49660ecd5c7320
Mateusz Magiera <mateuszmagiera122@gmail.com>
2020-04-19 20:40:04 +0200

    Punkt 4 gotowe

---
+++ Pamiec.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSB_C_2020_Z1
+{
+    class Pamiec
+    {
+        internal static double Sprawdz(bool pamiec, double wynik)
+        {
+            double x;
+            if (pamiec)
+            {
+                return wynik;
+            }
+            else
+            {
+                Console.WriteLine("Podaj pierwszą liczbę");
+                x = double.Parse(Console.ReadLine());
+                return x;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,54 +7,58 @@ namespace WSB_C_2020_Z1
         static void Main(string[] args)
         {
             bool dziala = true;
+            bool pamiec = false;
+            double wynik = 0;
 
-            while (dziala)
+            do
             {
                 string wybór;
-                double a, b, x;
+                double a, b;
+                
                 Console.WriteLine("Witaj w kalkulatorze. Wybierz co chcesz zrobić");
+
+                Console.WriteLine("\nWyświetlacz: " + wynik + "\n");
+
                 Console.WriteLine("1. Dodawanie");
                 Console.WriteLine("2. Odejmowanie");
                 Console.WriteLine("3. Mnożenie");
                 Console.WriteLine("4. Dzielenie");
-                Console.WriteLine("x. Zakończ program");
+                Console.WriteLine("x. Wynik");
 
                 wybór = Console.ReadLine();
-                if (wybór == "1")
-                {
-                    Console.WriteLine("Wybrano operację dodawania");
-                    Console.WriteLine("Podaj pierwszą liczbę");
-                    a = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Podaj drugą liczbę");
-                    b = double.Parse(Console.ReadLine());
-                    x = a + b;
-                    Console.WriteLine("wynik to " + x);
-                }
-                if (wybór == "2")
-                {
-                    Console.WriteLine("Wybrano operację odejmowania");
-                    Console.WriteLine("Podaj pierwszą liczbę:");
-                    a = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Podaj drugą liczbę:");
-                    b = double.Parse(Console.ReadLine());
-                    x = a - b;
-                    Console.WriteLine("wynik to " + x);
-                }
+
                 switch (wybór)
                 {
-                    case "3":
-                        Console.WriteLine("Wybrano operację mnożenia");
-                        Console.WriteLine("Podaj pierwszą liczbę:");
-                        a = double.Parse(Console.ReadLine());
+                    case "1":
+                        Console.WriteLine("Wybrano operację dodawania");
+                        a = Pamiec.Sprawdz(pamiec, wynik);
+                        Console.WriteLine("Podaj drugą liczbę");
+                        b = double.Parse(Console.ReadLine());
+                        wynik = a + b;
+                        pamiec = true;
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.WriteLine("Wybrano operację odejmowania");
+                        a = Pamiec.Sprawdz(pamiec, wynik);
                         Console.WriteLine("Podaj drugą liczbę:");
                         b = double.Parse(Console.ReadLine());
-                        x = a * b;
-                        Console.WriteLine("wynik to " + x);
+                        wynik = a - b;
+                        pamiec = true;
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Console.WriteLine("Wybrano operację mnożenia");
+                        a = Pamiec.Sprawdz(pamiec, wynik);
+                        Console.WriteLine("Podaj drugą liczbę:");
+                        b = double.Parse(Console.ReadLine());
+                        wynik = a * b;
+                        pamiec = true;
+                        Console.Clear();
                         break;
                     case "4":
                         Console.WriteLine("Wybrano operację dzielenia");
-                        Console.WriteLine("Podaj pierwszą liczbę:");
-                        a = double.Parse(Console.ReadLine());
+                        a = Pamiec.Sprawdz(pamiec, wynik);
                         do
                         {
                             Console.WriteLine("Podaj drugą liczbę:");
@@ -64,17 +68,22 @@ namespace WSB_C_2020_Z1
                                 Console.WriteLine("Nie można dzielić przez zero!");
                             }
                         } while (b == 0);
-                        x = a / b;
-                        Console.WriteLine("wynik to " + x);
+                        wynik = a / b;
+                        pamiec = true;
+                        Console.Clear();
                         break;
                     case "x":
                         dziala = false;
+                        Console.WriteLine("Wynik to: " + wynik);
+                        Console.ReadLine();
                         break;
                     default:
-                        Console.WriteLine("Nieznane polecenie.");
+                        Console.WriteLine("Nieznane polecenie. Wybierz ponownie.");
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
                 }
-            }
+            } while (dziala);
         }
     }
 }
