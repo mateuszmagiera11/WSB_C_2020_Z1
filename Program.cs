2e5d91f8cbcea3def0dc3b5bb0448e4dd73cf9dd
Mateusz Magiera <mateuszmagiera122@gmail.com>
2020-04-19 19:39:33 +0200

    punkt 4 program zapętlony w wyłącznikiem

--- Program.cs
+++ Program.cs
@@ -6,68 +6,75 @@ namespace WSB_C_2020_Z1
     {
         static void Main(string[] args)
         {
-            string wybór;
-            double a, b, x;
-            Console.WriteLine("Witaj w kalkulatorze. Wybierz co chcesz zrobić");
-            Console.WriteLine("1. Dodawanie");
-            Console.WriteLine("2. Odejmowanie");
-            Console.WriteLine("3. Mnożenie");
-            Console.WriteLine("4. Dzielenie");
-            
-            wybór = Console.ReadLine();
-            if (wybór == "1")
+            bool dziala = true;
+
+            while (dziala)
             {
-                Console.WriteLine("Wybrano operację dodawania");
-                Console.WriteLine("Podaj pierwszą liczbę");
-                a = double.Parse(Console.ReadLine());
-                Console.WriteLine("Podaj drugą liczbę");
-                b = double.Parse(Console.ReadLine());
-                x = a + b;
-                Console.WriteLine("wynik to " + x);
-            }
-            if (wybór == "2")
-            {
-                Console.WriteLine("Wybrano operację odejmowania");
-                Console.WriteLine("Podaj pierwszą liczbę:");
-                a = double.Parse(Console.ReadLine());
-                Console.WriteLine("Podaj drugą liczbę:");
-                b = double.Parse(Console.ReadLine());
-                x = a - b;
-                Console.WriteLine("wynik to " + x);
-            }
-            switch (wybór)
-            {
-                case "3":
-                    Console.WriteLine("Wybrano operację mnożenia");
+                string wybór;
+                double a, b, x;
+                Console.WriteLine("Witaj w kalkulatorze. Wybierz co chcesz zrobić");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                Console.WriteLine("3. Mnożenie");
+                Console.WriteLine("4. Dzielenie");
+                Console.WriteLine("x. Zakończ program");
+
+                wybór = Console.ReadLine();
+                if (wybór == "1")
+                {
+                    Console.WriteLine("Wybrano operację dodawania");
+                    Console.WriteLine("Podaj pierwszą liczbę");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj drugą liczbę");
+                    b = double.Parse(Console.ReadLine());
+                    x = a + b;
+                    Console.WriteLine("wynik to " + x);
+                }
+                if (wybór == "2")
+                {
+                    Console.WriteLine("Wybrano operację odejmowania");
                     Console.WriteLine("Podaj pierwszą liczbę:");
                     a = double.Parse(Console.ReadLine());
                     Console.WriteLine("Podaj drugą liczbę:");
                     b = double.Parse(Console.ReadLine());
-                    x = a * b;
+                    x = a - b;
                     Console.WriteLine("wynik to " + x);
-                    break;
-                case "4":
-                    Console.WriteLine("Wybrano operację dzielenia");
-                    Console.WriteLine("Podaj pierwszą liczbę:");
-                    a = double.Parse(Console.ReadLine());
-                    do
-                    {
+                }
+                switch (wybór)
+                {
+                    case "3":
+                        Console.WriteLine("Wybrano operację mnożenia");
+                        Console.WriteLine("Podaj pierwszą liczbę:");
+                        a = double.Parse(Console.ReadLine());
                         Console.WriteLine("Podaj drugą liczbę:");
                         b = double.Parse(Console.ReadLine());
-                        if (b == 0)
+                        x = a * b;
+                        Console.WriteLine("wynik to " + x);
+                        break;
+                    case "4":
+                        Console.WriteLine("Wybrano operację dzielenia");
+                        Console.WriteLine("Podaj pierwszą liczbę:");
+                        a = double.Parse(Console.ReadLine());
+                        do
                         {
-                            Console.WriteLine("Nie można dzielić przez zero!");
-                        }
-                    } while (b == 0);
-                    x = a / b;
-                    Console.WriteLine("wynik to " + x);
-                    break;
-                default:
-                    Console.WriteLine("Nieznane polecenie.");
-                    break;
+                            Console.WriteLine("Podaj drugą liczbę:");
+                            b = double.Parse(Console.ReadLine());
+                            if (b == 0)
+                            {
+                                Console.WriteLine("Nie można dzielić przez zero!");
+                            }
+                        } while (b == 0);
+                        x = a / b;
+                        Console.WriteLine("wynik to " + x);
+                        break;
+                    case "x":
+                        dziala = false;
+                        break;
+                    default:
+                        Console.WriteLine("Nieznane polecenie.");
+                        break;
+                }
             }
-
-            Console.ReadLine();
         }
     }
 }
