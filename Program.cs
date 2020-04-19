fa60f7f41196a8afd714d4389ec54b4ab8fc6e2b
Mateusz Magiera <mateuszmagiera122@gmail.com>
2020-04-19 18:31:23 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WSB_C_2020_Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
