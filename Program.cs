cfe7cf6aa8fb9edc1159867b62cc51658a53e73b
Mateusz Magiera <mateuszmagiera122@gmail.com>
2020-04-19 19:22:46 +0200

    Punkt 3_2 gotowe

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace WSB_C_2020_Z1
             Console.WriteLine("Witaj w kalkulatorze. Wybierz co chcesz zrobić");
             Console.WriteLine("1. Dodawanie");
             Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
             
             wybór = Console.ReadLine();
             if (wybór == "1")
@@ -32,6 +33,21 @@ namespace WSB_C_2020_Z1
                 b = double.Parse(Console.ReadLine());
                 x = a - b;
                 Console.WriteLine("wynik to " + x);
+            }
+            switch (wybór)
+            {
+                case "3":
+                    Console.WriteLine("Wybrano operację mnożenia");
+                    Console.WriteLine("Podaj pierwszą liczbę:");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj drugą liczbę:");
+                    b = double.Parse(Console.ReadLine());
+                    x = a * b;
+                    Console.WriteLine("wynik to " + x);
+                    break;
+                default:
+                    Console.WriteLine("Nieznane polecenie.");
+                    break;
             }
 
             Console.ReadLine();
